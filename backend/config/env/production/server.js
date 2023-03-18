5c2e0fc0ce206a7a734eb6ea82f69806c6d5e7e2
nurrizkyimn <nurrizkyimanidev@gmail.com>
2023-03-18 20:26:11 +0700

    commit before migration

--- backend/config/env/production/server.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({ env }) => ({
-  url: env("HEROKU_URL"),
-});
-
-// module.exports = ({ env }) => ({
-//   host: env("HOST", "0.0.0.0"),
-//   port: env.int("PORT", 1337),
-// });
