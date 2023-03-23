fc0afe1a1fd3226de9b41f57dfcf47ac26674f34
simonsfoundation <nurrizkyimani@mail.ugm.ac.id>
2023-03-23 20:42:01 +0700

    refactor middlewares.js and plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = ({ env }) => ({});
+const path = require("path");
+
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
+      providerOptions: {
+        serviceAccount: env("GCS_SERVICE_ACCOUNT"),
+        bucketName: env("GCS_BUCKET_NAME"),
+        baseUrl: `https://storage.googleapis.com/${env("GCS_BUCKET_NAME")}`,
+      },
+    },
+  },
+});
